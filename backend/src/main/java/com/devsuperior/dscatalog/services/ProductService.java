bf70189ac03f1c281c114ceb164a3f1fa8c4097d
wellington-workstation <rodrigueswellington3@gmail.com>
2024-07-25 20:09:15 -0300

    Factory, save test

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -65,9 +65,6 @@ public class ProductService {
 
     @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
-        if (!repository.existsById(id)) {
-            throw new ResourceNotFoundException("Recurso não encontrado");
-        }
         try {
             repository.deleteById(id);
         } catch (DataIntegrityViolationException e) {
