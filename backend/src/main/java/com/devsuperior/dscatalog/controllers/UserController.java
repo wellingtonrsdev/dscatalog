dfcd45bab8d33cc5605bc182032f35d67bab5600
wellington-workstation <rodrigueswellington3@gmail.com>
2024-09-20 16:09:01 -0300

    Caso de uso Signup implementado

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/UserController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/UserController.java
@@ -38,9 +38,8 @@ public class UserController {
         return ResponseEntity.ok().body(dto);
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
+    public ResponseEntity<UserDTO> insert(@RequestBody @Valid UserInsertDTO dto) {
         UserDTO newDto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,10 +1,14 @@
 package com.devsuperior.dscatalog.dto;
 
 import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
+    @NotBlank(message = "Campo obrigatório")
+    @Size(min = 8, message = "Deve ter no mínimo 08 caracteres")
     private String password;
 
     public UserInsertDTO() {
