b9d26defb7d5358e09655add6b9b6ad67a11bb4e
wellington-workstation <rodrigueswellington3@gmail.com>
2024-09-20 17:58:09 -0300

    Preparação do projeto para caso de uso de recuperação de Email

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/EmailService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.services.exceptions.EmailException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	@Value("${spring.mail.username}")
+	private String emailFrom;
+
+    private final JavaMailSender emailSender;
+
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendEmail(String to, String subject, String body) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailFrom);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            emailSender.send(message);
+        } 
+        catch (MailException e){
+        	throw new EmailException("Failed to send email");
+        } 
+    }
+}
