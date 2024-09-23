3c521d69bc85176a368776379f14fc6fbbf66df9
wellington-workstation <rodrigueswellington3@gmail.com>
2024-09-23 16:38:48 -0300

    Endpoint de recuperação de email e validação

---
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dscatalog.controllers;
+
+import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
+import com.devsuperior.dscatalog.services.AuthService;
+import com.devsuperior.dscatalog.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = "/recover-token")
+    public ResponseEntity<Void> createRecoverToken(@Valid @RequestBody EmailDTO body) {
+        authService.createRecoverToken(body);
+        return ResponseEntity.noContent().build();
+    }
+
+}
